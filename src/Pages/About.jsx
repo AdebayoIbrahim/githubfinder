5b51021d017d035293ea8481971bde189cc406e3
SetUp <abdulmumeenibroheem51@gmail.com>
2023-05-30 16:47:09 +0100

    implementing react helmet for custom route title

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { Typography } from "@mui/material";
+import { Helmet } from "react-helmet";
 function About() {
   return (
     <div>
-      <Typography component="h1" color="white" sx={{ fontSize: "2rem" }}>
-        Github Finder
-      </Typography>
-      <Typography component="p" variant="span">
-        A React App That Uses Github Rest Api To Find Github Users
-      </Typography>
-      <Typography component="p" variant="span" color="grey" mt={2}>
-        Version:{" "}
-        <Typography component="span" variant="caption">
-          1.0.0
+      <Helmet>
+        <title>About</title>
+        <Typography component="h1" color="white" sx={{ fontSize: "2rem" }}>
+          Github Finder
         </Typography>
-      </Typography>
-      <Typography component="p" variant="span" color="grey">
-        Originally By:{" "}
-        <Typography component="span" variant="caption">
-          <a href="http://www.traversymedia.com">Brad Traversy</a>
+        <Typography component="p" variant="span">
+          A React App That Uses Github Rest Api To Find Github Users
         </Typography>
-      </Typography>
+        <Typography component="p" variant="span" color="grey" mt={2}>
+          Version:{" "}
+          <Typography component="span" variant="caption">
+            1.0.0
+          </Typography>
+        </Typography>
+        <Typography component="p" variant="span" color="grey">
+          Originally By:{" "}
+          <Typography component="span" variant="caption">
+            <a href="http://www.traversymedia.com">Brad Traversy</a>
+          </Typography>
+        </Typography>
+      </Helmet>
     </div>
   );
 }
