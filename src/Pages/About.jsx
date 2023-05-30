d07b32dd16468bb5517bed7f1cbb1a0b38531aa1
SetUp <abdulmumeenibroheem51@gmail.com>
2023-05-30 17:33:29 +0100

    fixed helmet tags error in about.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,25 +6,25 @@ function About() {
     <div>
       <Helmet>
         <title>About</title>
-        <Typography component="h1" color="white" sx={{ fontSize: "2rem" }}>
-          Github Finder
-        </Typography>
-        <Typography component="p" variant="span">
-          A React App That Uses Github Rest Api To Find Github Users
-        </Typography>
-        <Typography component="p" variant="span" color="grey" mt={2}>
-          Version:{" "}
-          <Typography component="span" variant="caption">
-            1.0.0
-          </Typography>
-        </Typography>
-        <Typography component="p" variant="span" color="grey">
-          Originally By:{" "}
-          <Typography component="span" variant="caption">
-            <a href="http://www.traversymedia.com">Brad Traversy</a>
-          </Typography>
-        </Typography>
       </Helmet>
+      <Typography component="h1" color="white" sx={{ fontSize: "2rem" }}>
+        Github Finder
+      </Typography>
+      <Typography component="p" variant="span">
+        A React App That Uses Github Rest Api To Find Github Users
+      </Typography>
+      <Typography component="p" variant="span" color="grey" mt={2}>
+        Version:{" "}
+        <Typography component="span" variant="caption">
+          1.0.0
+        </Typography>
+      </Typography>
+      <Typography component="p" variant="span" color="grey">
+        Originally By:{" "}
+        <Typography component="span" variant="caption">
+          <a href="http://www.traversymedia.com">Brad Traversy</a>
+        </Typography>
+      </Typography>
     </div>
   );
 }
--- src/Pages/Notfound.jsx
+++ src/Pages/Notfound.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 
 const Notfound = () => {
-  return <Box sx={{ textAlign: "center" }}></Box>;
+  return (
+    <Box sx={{ textAlign: "center" }}>
+      <Typography component="h1" color="white" sx={{ fontSize: "2.4rem" }}>
+        Error 404!
+      </Typography>
+    </Box>
+  );
 };
 
 export default Notfound;
