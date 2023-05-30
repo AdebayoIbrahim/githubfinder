7fdb9d0b70f5a556c41f2f2b3887b3fc54b65101
SetUp <abdulmumeenibroheem51@gmail.com>
2023-05-30 17:50:48 +0100

    not_found page

--- src/Pages/Notfound.jsx
+++ src/Pages/Notfound.jsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
-
+import { Box, Typography, Button } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import { Link } from "react-router-dom";
 const Notfound = () => {
   return (
     <Box sx={{ textAlign: "center" }}>
       <Typography component="h1" color="white" sx={{ fontSize: "2.4rem" }}>
         Error 404!
       </Typography>
+      <Typography component="p" variant="h6" color="white">
+        The Page You Are Looking For Does Not Exist!
+      </Typography>
+      <Box pt={2}>
+        <Link to="/">
+          <Button
+            startIcon={<HomeIcon />}
+            variant="contained"
+            sx={{ background: "green" }}
+          >
+            Back to Home
+          </Button>
+        </Link>
+      </Box>
     </Box>
   );
 };
