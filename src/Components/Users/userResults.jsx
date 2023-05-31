ac85c112d9e62055cdaf1b2ab6ed423b521e7044
SetUp <abdulmumeenibroheem51@gmail.com>
2023-05-31 18:06:48 +0100

    adding .env folder and fetching data from api

--- src/Components/Users/userResults.jsx
+++ src/Components/Users/userResults.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 const UserResults = () => {
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState([]);
   useEffect(() => {
     fetchData();
   }, []);
@@ -13,9 +15,27 @@ const UserResults = () => {
     });
     const result = await response.json();
     console.log(result);
+    setUsers(result);
+    setLoading(false);
   };
 
-  return <div>userResults</div>;
+  return loading ? (
+    <h2>Loading....</h2>
+  ) : (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        gap: "1rem",
+        alignItems: "center",
+      }}
+    >
+      {users.map((item) => {
+        return <p>{item.login}</p>;
+      })}
+    </div>
+  );
 };
 
 export default UserResults;
