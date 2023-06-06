dd5a64301c1b8ae401e273758ae01b54b8c85c99
SetUp <abdulmumeenibroheem51@gmail.com>
2023-06-06 17:20:19 +0100

    search from validation

--- src/Components/Users/userSearch.jsx
+++ src/Components/Users/userSearch.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Stack, Box, Button } from "@mui/material";
 const UserSearch = () => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    console.log(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value === "") {
+      alert("Please input something");
+    } else {
+      // send request
+    }
+  };
+
   return (
     <Box pb={5} pl={7} sx={{ marginInline: "3%" }}>
       <Stack gap={2} direction="row" width="100%">
         <Box flex={{ xs: 15, sm: 5 }}>
           <Box>
-            <form>
+            <form onSubmit={handleSubmit}>
               <Box sx={{ width: "100%" }}>
                 <Stack direction="row">
                   <input
                     placeholder="Search.."
                     type="text"
                     id="outlined-search"
+                    value={value}
+                    onChange={handleChange}
                   />
                   <Button
                     type="submit"
